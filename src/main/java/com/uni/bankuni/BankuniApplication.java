b7b1ed3c71c4c551862b5a2afca881819cb1f435
sjellters <andradec.diego@gmail.com>
2021-02-16 23:17:05 -0500

    Setting up dependencies

---
+++ src/main/java/com/uni/bankuni/BankuniApplication.java
@@ -0,0 +1,13 @@
+package com.uni.bankuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankuniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankuniApplication.class, args);
+    }
+
+}
